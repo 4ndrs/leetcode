c2188eca80088f1809ae9af1a2590d3b0628a5af
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-19 02:29:19 +0100

    Arrays & Hashing: Add `Two Sum` modifications

--- src/Arrays & Hashing/Two Sum/solution.ts
+++ src/Arrays & Hashing/Two Sum/solution.ts
@@ -1,11 +1,20 @@
 const twoSum = (numbers: number[], target: number) => {
-  for (let outer = 0; outer < numbers.length; outer++) {
-    for (let inner = outer + 1; inner < numbers.length; inner++) {
-      if (numbers[outer] + numbers[inner] === target) {
-        return [outer, inner];
-      }
+  const map = new Map<number, number>();
+  const result: number[] = [];
+
+  for (const [index, current] of numbers.entries()) {
+    const number = target - current;
+    const found = map.get(number);
+
+    if (found !== undefined) {
+      result.push(found, index);
+      break;
     }
+
+    map.set(current, index);
   }
+
+  return result;
 };
 
 export { twoSum };
